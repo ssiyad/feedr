788549c229418f6761722200c5b5dbd3baeecf48
Sabu Siyad <hello@ssiyad.com>
2024-02-18 23:27:00 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Geist', 'sans-serif'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
